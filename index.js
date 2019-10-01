e981f2b0c12c8355d2bc660d23021a90cf2644ca
elijahmm17 <elijahmm17@gmail.com>
2019-10-01 22:03:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+
+function retrieveEmployeeInformation() {
+  
+}
+
+function addNewElementAsLi() {
+  
+}
+
+function addNewLiOnClick() {
+  
+}
+
+function clearEmployeeListOnLinkClick() {
+  
+}
